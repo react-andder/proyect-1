20fa2cb9bbc71dcf69f212e3b7d67dd5637f12ab
andder <kiritooo956@gmail.com>
2022-12-24 22:03:39 -0500

    subiendo proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 
-const app = <h1>Hola Mundo</h1>;
+const app = <h1>Hola </h1>;
 const container = document.getElementById('root');
 
 createRoot(container).render(app);
